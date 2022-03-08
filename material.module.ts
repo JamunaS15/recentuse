bd445af0e1bd9541b3b8b50dccf996e79928727b
JamunaS15 <63998531+JamunaS15@users.noreply.github.com>
2022-03-08 10:54:52 +0530

    Add files via upload

---
+++ material.module.ts
@@ -0,0 +1,98 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatInputModule } from '@angular/material/input'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { GoogleChartsModule } from 'angular-google-charts';
+import {MatButtonToggleModule} from '@angular/material/button-toggle'; 
+import {MatTableModule} from '@angular/material/table'; 
+import {MatSortModule} from '@angular/material/sort'; 
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatSelectModule} from '@angular/material/select'; 
+import {MatProgressBarModule} from '@angular/material/progress-bar'; 
+import {MatSlideToggleModule} from '@angular/material/slide-toggle'; 
+import { MomentModule } from 'ngx-moment';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDialogModule} from '@angular/material/dialog'; 
+import {MatCheckboxModule} from '@angular/material/checkbox'; 
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableExporterModule } from 'mat-table-exporter';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatCardModule, 
+    FormsModule,
+    MatInputModule, 
+    MatIconModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatTabsModule,
+    FlexLayoutModule,
+    GoogleChartsModule,
+    MatButtonToggleModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatPaginatorModule,
+    MatSlideToggleModule,
+    MomentModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatSnackBarModule,
+    MatTableExporterModule,
+    MatProgressSpinnerModule
+  ],
+  exports: [
+    CommonModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatTabsModule,
+    FlexLayoutModule,
+    GoogleChartsModule,
+    MatButtonToggleModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatPaginatorModule,
+    MatSlideToggleModule,
+    MomentModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatSnackBarModule,
+    MatTableExporterModule,
+    MatProgressSpinnerModule
+  ]
+})
+export class MaterialModule { }
